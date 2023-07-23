275d08aed07db9d3f439429241584244d112dd55
Ole <117225602+olebol@users.noreply.github.com>
2023-07-23 22:55:28 +0200

    Added the "has taken fork" message back.

--- src/routine.c
+++ src/routine.c
@@ -12,19 +12,41 @@
 
 #include "philosophers.h"
 
+static int	lock_forks(t_philo *philo)
+{
+	pthread_mutex_lock(philo->left_fork);
+	if (!print_update(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(philo->left_fork);
+		return (0);
+	}
+	pthread_mutex_lock(philo->right_fork);
+	if (!print_update(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
+		return (0);
+	}
+}
+
+static void	unlock_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
+
 static int	eat(t_philo *philo)
 {
 	if (!print_update(philo, "is thinking"))
 		return (0);
-	pthread_mutex_lock(philo->left_fork);
-	pthread_mutex_lock(philo->right_fork);
+	if (lock_forks(philo))
+		return (0);
 	pthread_mutex_lock(&philo->last_eat_mutex);
 	philo->time_last_eat = get_time();
 	pthread_mutex_unlock(&philo->last_eat_mutex);
 	if (!print_update(philo, "is eating"))
 	{
-		pthread_mutex_unlock(philo->left_fork);
-		pthread_mutex_unlock(philo->right_fork);
+		unlock_forks();
 		return (0);
 	}
 	if (philo->shared->times_to_eat)
@@ -34,8 +56,7 @@ static int	eat(t_philo *philo)
 		pthread_mutex_unlock(&philo->eat_mutex);
 	}
 	ft_sleep(philo->shared->eat_time);
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
+	unlock_forks();
 	return (1);
 }
 
